f2537db081b2b9dd2bf2ca15cb3401825ae62736
Greggk <ygk@qwest.net>
2021-12-26 13:35:29 -0700

    Change plugin (tools) extentions to .cmd and .btm

--- src/util/pluginmanager.cpp
+++ src/util/pluginmanager.cpp
@@ -1,4 +1,5 @@
 #include "pluginmanager.h"
+#include <iostream>
 
 PluginManager::PluginManager()
 {
@@ -6,7 +7,11 @@ PluginManager::PluginManager()
     filesystem::create_directory(dir);
     for (filesystem::directory_entry e : filesystem::directory_iterator(TOOLS_FOLDER)) {
         filesystem::path p = e.path();
+#ifdef __OS2__
+        if ((p.extension() == ".cmd") || (p.extension() == ".btm"))
+#else
         if ((p.extension() == ".sh") || (p.extension() == ".bat"))
+#endif
             this->tools.push_back(p.filename());
     }
 }
